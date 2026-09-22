17157ab1417d1ec0a935833b2075231cdb8cadee
kadamyash853 <yashkadam4407@gmail.com>
2026-09-22 18:13:00 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x=10;
+char y='b';
+x=x+y;
+float z=x+1.0;
+cout<<"x="<<x<<endl;
+cout<<"y="<<y<<endl;
+cout<<"z="<<z<<endl;
+return 0;
+}
