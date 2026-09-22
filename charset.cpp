4f692b9899f477c5bc6f0ca49d68a30d095d0608
kadamyash853 <yashkadam4407@gmail.com>
2026-09-22 18:14:26 +0530

    Add files via upload

---
+++ charset.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+char letter, digit , special ;
+cout<<"enter the letter";
+cin>>letter;
+cout<<"you entered letter :"<<letter<<endl;
+cout<<"enter the digit";
+cin>>digit;
+cout<<"you entered digit:"<<digit<<endl;
+cout<<"enter a special";
+cin>>special;
+cout<<"you entered special"<<special<<endl;
+return 0;
+}
