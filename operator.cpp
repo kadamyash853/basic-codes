bca4b54241e75b2daff05ad9a91ef28b32ccb96c
kadamyash853 <yashkadam4407@gmail.com>
2026-09-22 18:13:32 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+int a=5;
+int b=10;
+int c=a+b;
+cout<<"the sum of "<<a<<"and"<<b<<"is"<<c<<endl;
+c=a-b;
+cout<<"the difference between "<<a<<"amd"<<b<<"is"<<c<<endl;
+c=a*b;
+cout<<"the product of "<<a<<"and"<<b<<"is"<<c<<endl;
+c=a/b;
+cout<<"the quotient of "<<a<<"and" <<b<<"is"<<c<<endl;
+return 0;
+}
